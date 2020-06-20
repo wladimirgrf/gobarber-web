ea3eced3f17a58ca650bd44ac55a45d15fdc6205
Wladimir <wladimir.code@gmail.com>
2020-06-20 17:17:35 -0300

    Global Style, styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FunctionComponent = () => <h1>Teste</h1>;
+import GlobalStyle from './styles/global';
+
+const App: React.FunctionComponent = () => (
+  <>
+    <h1>Teste</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
