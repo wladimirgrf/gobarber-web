c68c9731e20972fa073a50f8c2743c805168ee8b
Wladimir <wladimir.code@gmail.com>
2020-10-06 20:24:12 -0300

    feat: Dashboard calendar, react-day-picker

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,8 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+
+import 'react-day-picker/lib/style.css';
+
 import { FiPower, FiClock } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
@@ -21,6 +25,12 @@ const Dashboard: React.FunctionComponent = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
 
   const { signOut, user } = useAuth();
+
+  const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
 
   return (
     <Container>
@@ -125,7 +135,15 @@ const Dashboard: React.FunctionComponent = () => {
             </Appointment>
           </Section>
         </Schedule>
-        <Calendar />
+        <Calendar>
+          <DayPicker
+            onDayClick={handleDateChange}
+            selectedDays={selectedDate}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{ available: { daysOfWeek: [1, 2, 3, 4, 5] } }}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
