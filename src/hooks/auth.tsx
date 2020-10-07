dc76eefefb2201088c3b179fcea252ef71b7a7e9
Wladimir <wladimir.code@gmail.com>
2020-10-06 21:31:29 -0300

    feat: Authorization request token, signIn

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -32,8 +32,11 @@ const AuthProvider: React.FunctionComponent = ({ children }) => {
     const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
+
     return {} as AuthState;
   });
 
@@ -47,6 +50,8 @@ const AuthProvider: React.FunctionComponent = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
