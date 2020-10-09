286ecd1732b6d12911f6d01239da0f5269d9bcec
Wladimir <wladimir.code@gmail.com>
2020-10-08 22:38:42 -0300

    add: Dashboard, profile link

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState, useEffect, useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import { format, parseISO, isToday, isAfter } from 'date-fns';
 
@@ -142,7 +143,9 @@ const Dashboard: React.FunctionComponent = () => {
 
             <div>
               <span>Welcome,</span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
